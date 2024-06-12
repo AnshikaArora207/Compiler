afd37ae591c2070d6e388cf0a0ea9ea1b10e7e7f
Anshi <anshikaa207@gmail.com>
2024-06-12 13:08:48 +0530

    save button working

--- client/src/components/HelperHeader.tsx
+++ client/src/components/HelperHeader.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Button } from './ui/button'
-import { Save, Share2 } from 'lucide-react'
+import { Fullscreen, Save, Share2 } from 'lucide-react'
 import {
     Select,
     SelectContent,
@@ -11,14 +11,26 @@ import {
 import { useDispatch, useSelector } from 'react-redux'
 import { CompilerSliceStateType, updateCurrentLanguage } from '@/Redux/slices/compilerSlice'
 import { RootState } from '@/Redux/store'
+import { handleError } from '@/utils/handleError'
+import axios from 'axios'
 
 const HelperHeader = () => {
+  const fullCode = useSelector((state:RootState)=>state.compilerSlice.fullCode);
+  const handleSave = async()=>{
+    try {
+      const response = await axios.post("http://localhost:4000/compiler/save",{
+        fullCode: fullCode
+      })
+    } catch (error) {
+      handleError(error);
+    }
+  }
     const dispatch = useDispatch()
     const currentLanguage = useSelector((state:RootState)=>state.compilerSlice.currentLanguage)
   return (
     <div className='_helper_header h-[50px] bg-black text-white p-2 flex justify-between items-center'>
         <div className='_btn_container flex gap-1'>
-            <Button className='flex justify-center items-center gap-1' variant="success"><Save size={16} />Save</Button>
+            <Button onClick={handleSave} className='flex justify-center items-center gap-1' variant="success"><Save size={16} />Save</Button>
             <Button className='flex justify-center items-center gap-1' variant="secondary"><Share2 size={16}/>Share</Button>
         </div>
         <div className='_tab_switcher flex justify-center items-center gap-1'>
