cf8ca5892ed71a75737eb0d2c6c8d5d0b7f2757e
Anshika Arora <124586347+AnshikaArora207@users.noreply.github.com>
2024-06-14 12:00:15 +0530

    experimenting with compilerRouter.ts

--- server/src/routes/compilerRouter.ts
+++ server/src/routes/compilerRouter.ts
@@ -2,5 +2,8 @@ import express from "express";
 import {saveCode,loadCode} from "../controllers/compilerController";
 
 export const compilerRouter = express.Router();
+compilerRouter.get("/",(req,res)=>{
+  return res.send("reached");
+});
 compilerRouter.post("/save",saveCode);
 compilerRouter.post("/load",loadCode);
